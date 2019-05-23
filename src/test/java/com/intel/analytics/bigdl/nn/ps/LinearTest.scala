6f63a4f6b60a297d8a201a943fd3f4602fda4a29
yaochi <yaochi@sugo.io>
2019-05-23 17:52:27 +0800

    add a empty unit for layers

---
+++ src/test/java/com/intel/analytics/bigdl/nn/ps/LinearTest.scala
@@ -0,0 +1,13 @@
+package com.intel.analytics.bigdl.nn.ps
+
+import org.junit.{Assert, Test}
+
+class LinearTest extends Assert {
+  @Test
+  def testBuild(): Unit = {
+  }
+
+  @Test
+  def testForward(): Unit = {
+  }
+}
---
+++ src/test/java/com/intel/analytics/bigdl/nn/ps/LookupTableTest.scala
@@ -0,0 +1,13 @@
+package com.intel.analytics.bigdl.nn.ps
+
+import org.junit.{Assert, Test}
+
+class LookupTableTest extends Assert {
+  @Test
+  def testBuild(): Unit = {
+  }
+
+  @Test
+  def testForward(): Unit = {
+  }
+}
